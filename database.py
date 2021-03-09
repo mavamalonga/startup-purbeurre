e0f973dc9084bf28aa922b8e9a2160f83c3efebf
Alpha <mavamalonga.alpha@gmail.com>
2021-03-09 06:58:30 +0100

    create script database

--- database.py
+++ database.py
@@ -2,3 +2,26 @@ import mysql.connector
 from mysql.connector.cursor import MySQLCursorPrepared
 
 import recover_data_api
+
+class Database:
+
+	def __init__(self):
+
+		self.Id = mysql.connector.connect(user='root', host='localhost',
+	password='100ml80%vol.', port='330', database='person')
+		self.cursor = self.Id.cursor()
+
+	def create_database(self):
+
+		with open("base_ap.sql", "r") as f:
+			base = f.read()
+			self.cursor.execute(base)
+
+	def connect_database(self):
+
+		self.Id = mysql.connector.connect(user='root', host='localhost',
+	password='100ml80%vol.', port='330', database='person')
+		self.cursor = self.Id.cursor()
+		self.cursor.execute("use Purbeurre")
+
+	
