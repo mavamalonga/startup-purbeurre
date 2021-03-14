117252395842de614bed9002f7a0911bb15e46d0
Alpha <mavamalonga.alpha@gmail.com>
2021-03-14 08:22:34 +0100

    update file view

--- vue.py
+++ vue.py
@@ -2,20 +2,21 @@ import requests
 import mysql.connector
 from mysql.connector.cursor import MySQLCursorPrepared
 from mysql.connector import errorcode
-import database
-import api
+from database import Database
+from api import get_data_api
 import tables
 
-def main():
+b = Database(tables.TABLES)
+c = get_data_api()
 
-	b = Database(tables.TABLES)
+def main(Database, api):
+
 	b.create_database()
 	b.create_table()
 
-	c = get_data_api()
 	c.load_data()
 	c.insert_category()
 	c.insert_data()
 
 
-main()
+main(b, c)
